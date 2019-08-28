49bb8aebffef998da116f98e4c71daaba8a2e081
Will Belote <wbelote@unca.edu>
2019-08-28 19:54:05 -0400

    add first listgame attempt

---
+++ listgame.py
@@ -0,0 +1,22 @@
+import math
+import sys
+
+
+def is_prime(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+def factors(x):
+    if is_prime(x):
+        return []
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            if is_prime(x // i):
+                return [i, x // i]
+            return [i] + factors(x // i)
+
+
+print(len(factors(int(sys.stdin.readline()))))
