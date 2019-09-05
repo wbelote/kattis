d076ee3ba07b22f4b138411187d8728e3ba933b8
Will Belote <wbelote@unca.edu>
2019-09-04 23:41:52 -0400

    changed zebrasocelots to second version

--- zebrasocelots.py
+++ zebrasocelots.py
@@ -3,15 +3,12 @@ import sys
 
 def main():
     n = int(sys.stdin.readline())
-    pile = [sys.stdin.readline()[0] for i in range(n)][::-1]
+    total = 0
+    for i in range(n - 1, -1, -1):
+        if sys.stdin.readline()[0] == "O":
+            total += 2 ** i
 
-    count = 0
-    while True:
-        if "O" not in pile:
-            print(count)
-            return
-        pos = pile.index("O")
-        pile = ["O"] * pos + ["Z"] + pile
+    print(total)
 
 
 main()
