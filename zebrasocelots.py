757f934ecbda5ef450b0c334d45857e3b8b7dae4
Will Belote <wbelote@unca.edu>
2019-09-04 23:40:43 -0400

    changed zebrasocelots to just first attempt

--- zebrasocelots.py
+++ zebrasocelots.py
@@ -14,14 +14,4 @@ def main():
         pile = ["O"] * pos + ["Z"] + pile
 
 
-def main2():
-    n = int(sys.stdin.readline())
-    total = 0
-    for i in range(n - 1, -1, -1):
-        if sys.stdin.readline()[0] == "O":
-            total += 2 ** i
-
-    print(total)
-
-
-main2()
+main()
