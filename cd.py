6c2f8c1632fb129148ff99d1c84d7496ef98e9c0
Will Belote <wbelote@unca.edu>
2019-09-07 00:20:13 -0400

    update cd with set-based attempt

--- cd.py
+++ cd.py
@@ -1,21 +1,21 @@
 import sys
 
-def main():
-    n = sys.stdin.readline().split()
-    jack = []
-    jill = []
-    
-    for i in range(int(n[0])):
-        jack.append(int(sys.stdin.readline()))
-        
-    for i in range(int(n[1])):
-        jill.append(int(sys.stdin.readline()))
 
-    both = 0
-    for cd in set(jack+jill):
-        if cd in jack and cd in jill:
-            both += 1
-    print(both)
-        
+def main():
+    while True:
+        line = sys.stdin.readline().split()
+        n, m = [int(x) for x in line]
+        if not n + m:
+            return
+        if not n or not m:
+            print(0)
+            continue
+
+        cds = [sys.stdin.readline() for i in range(n+m)]
+        jack = set(cds[:n])
+        jill = set(cds[n:])
+
+        print(len(jack & jill))
+
 
 main()
