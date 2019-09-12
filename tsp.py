a64381a31861988fbcf0ea6276036b7e58b5e839
Will Belote <wbelote@unca.edu>
2019-09-12 00:49:49 -0400

    added tsp

---
+++ tsp.py
@@ -0,0 +1,28 @@
+import sys, time, math, itertools
+
+start = time.time()
+
+
+def dist(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+
+
+n = int(sys.stdin.readline())
+stop = 2 - n / 1000
+points = tuple(tuple(float(x) for x in sys.stdin.readline().split()) for i in range(n))
+
+best_dist = sum([dist(points[i], points[i + 1]) for i in range(n - 1)])
+best_path = tuple(range(n))
+
+for path in itertools.permutations(range(n)):
+    path_points = tuple(points[x] for x in path)
+    path_dist = sum([dist(path_points[i], path_points[i + 1]) for i in range(n - 1)])
+    if path_dist < best_dist:
+        best_dist = path_dist
+        best_path = path
+
+    if time.time() - start >= stop:
+        for p in best_path:
+            print(p)
+        print(time.time() - start)
+        break
