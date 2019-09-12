6c9be9916d6d167c8d21e9d9bc6ca5dc8b931d90
Will Belote <wbelote@unca.edu>
2019-09-12 01:38:23 -0400

    updated tsp to working version

--- tsp.py
+++ tsp.py
@@ -4,16 +4,20 @@ start = time.time()
 
 
 def dist(a, b):
-    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+    return round(((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5)
 
 
 n = int(sys.stdin.readline())
-stop = 2 - n / 1000
+if n == 1:
+    print(0)
+    sys.exit(0)
+stop = 1.95 - n / 200000
 points = tuple(tuple(float(x) for x in sys.stdin.readline().split()) for i in range(n))
 
-best_dist = sum([dist(points[i], points[i + 1]) for i in range(n - 1)])
+best_dist = sum([dist(points[i], points[i - 1]) for i in range(n)])
 best_path = tuple(range(n))
 
+loops = 0
 for path in itertools.permutations(range(n)):
     path_points = tuple(points[x] for x in path)
     path_dist = sum([dist(path_points[i], path_points[i + 1]) for i in range(n - 1)])
@@ -24,5 +28,5 @@ for path in itertools.permutations(range(n)):
     if time.time() - start >= stop:
         for p in best_path:
             print(p)
-        print(time.time() - start)
+        # print(time.time() - start)
         break
