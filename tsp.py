9260805a0c9e755510f0b00f77aeb83174f47794
Will Belote <wbelote@unca.edu>
2019-09-12 01:42:21 -0400

    added loop counter to tsp

--- tsp.py
+++ tsp.py
@@ -1,4 +1,6 @@
-import sys, time, math, itertools
+import itertools
+import sys
+import time
 
 start = time.time()
 
@@ -19,6 +21,7 @@ best_path = tuple(range(n))
 
 loops = 0
 for path in itertools.permutations(range(n)):
+    loops += 1
     path_points = tuple(points[x] for x in path)
     path_dist = sum([dist(path_points[i], path_points[i + 1]) for i in range(n - 1)])
     if path_dist < best_dist:
@@ -28,5 +31,6 @@ for path in itertools.permutations(range(n)):
     if time.time() - start >= stop:
         for p in best_path:
             print(p)
-        # print(time.time() - start)
+        print(time.time() - start)
+        print(loops)
         break
