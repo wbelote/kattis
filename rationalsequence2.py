61de468a81e5321745839226cb41dc0a85d704b4
Will Belote <wbelote@unca.edu>
2019-09-17 19:06:30 -0400

    implemented rationalsequence2

--- rationalsequence2.py
+++ rationalsequence2.py
@@ -1,6 +1,32 @@
 import sys
 
 
+def parent(x):
+    a, b = x
+    if a < b:
+        return [a, b - a]
+    else:
+        return [a - b, b]
+
+
 def main():
     p = int(sys.stdin.readline())
+    for i in range(p):
+        line = sys.stdin.readline().split()
+        k = line[0]
+        a, b = [int(x) for x in line[1].split("/")]
+        path = []
+        now = [a, b]
+        while now != [1, 1]:
+            path = [now[0] > now[1]] + path
+            now = parent(now)
 
+        total = 1
+        for right in path:
+            total *= 2
+            total += right
+
+        print(k, total)
+
+
+main()
