67704326ffaabcc1d11c8e90a43494ab3d98005a
Will Belote <wbelote@unca.edu>
2019-10-04 02:12:17 -0400

    changed to graph search

--- 10kindsofpeople.py
+++ 10kindsofpeople.py
@@ -1,17 +1,65 @@
 import sys
 
 
+class Queue:
+    def __init__(self, size=1024):
+        self.data = [None] * size
+        self.start = 0
+        self.end = 0
+        self.size = size
+
+    def enq(self, val):
+        if (self.start - self.end) % self.size == 1:
+            new = [self.deq() for _ in range(self.size)] + [None] * self.size
+            self.data = new
+            self.start = 0
+            self.end = 0
+            self.size *= 2
+
+        self.data[self.end] = val
+        self.end += 1
+        self.end %= self.size
+
+    def deq(self):
+        if self.is_empty:
+            return None
+        out = self.data[self.start]
+        self.start += 1
+        self.start %= self.size
+        return out
+
+    @property
+    def is_empty(self):
+        return self.start == self.end
+
+
 def neighbors(pos, r, c, layout):
-    out = (pos,)
+    out = []
     if pos // c > 0 and layout[pos - c] == layout[pos]:
-        out += (pos - c,)
+        out += [pos - c, ]
     if pos // c < r - 1 and layout[pos + c] == layout[pos]:
-        out += (pos + c,)
+        out += [pos + c, ]
     if pos % c > 0 and layout[pos - 1] == layout[pos]:
-        out += (pos - 1,)
-    if pos % c < c - 1 and layout[pos - 1] == layout[pos]:
-        out += (pos + 1,)
+        out += [pos - 1, ]
+    if pos % c < c - 1 and layout[pos + 1] == layout[pos]:
+        out += [pos + 1, ]
     return out
+
+
+def search(graph, start, finish, size):
+    visited = set()
+    queue = Queue(size)
+    queue.enq(start)
+    while not queue.is_empty:
+        node = queue.deq()
+        if node == finish:
+            return True
+        adj = graph[node]
+        for n in adj:
+            if n not in visited:
+                queue.enq(n)
+        visited.add(node)
+    return False
 
 
 def main():
@@ -23,47 +71,12 @@ def main():
     n = int(sys.stdin.readline())
     queries = [[int(x) - 1 for x in sys.stdin.readline().split()] for _ in range(n)]
 
-    zone = [0] * rows * cols
-    max_zone = 0
-    actual = [0]
-
-    for pos in range(rows * cols):
-        paths = neighbors(pos, rows, cols, kind_map)
-        path_zones = {actual[zone[p]] for p in paths if zone[p]}
-        if not path_zones:
-            max_zone += 1
-            actual.append(max_zone)
-            for p in paths:
-                zone[p] = max_zone
-        else:
-            new = min(path_zones)
-            while new != actual[new]:
-                new = actual[new]
-            for p in paths:
-                if zone[p] != 0:
-                    actual[zone[p]] = new
-                zone[p] = new
-
-    update = [actual[x] for x in actual]
-    while actual != update:
-        actual = update
-        update = [actual[x] for x in actual]
-    actual = update
-
-    # print()
-    # for r in range(rows):
-    #     for c in range(cols):
-    #         print(chr(actual[zone[r * cols + c]] + 32), end=" ")
-    #         # print(actual[zone[r * cols + c]], end=" ")
-    #     print()
-    # print()
-    # print(actual)
-    # print()
+    graph = [neighbors(i, rows, cols, kind_map) for i in range(rows * cols)]
 
     for q in queries:
         start = q[0] * cols + q[1]
         end = q[2] * cols + q[3]
-        if actual[zone[start]] == actual[zone[end]]:
+        if search(graph, start, end, rows * cols):
             print(["binary", "decimal"][int(kind_map[start])])
         else:
             print("neither")
