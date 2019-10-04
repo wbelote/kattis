f6e262d7b008906752e4c24ed70ec84e191975bb
Will Belote <wbelote@unca.edu>
2019-10-04 19:32:28 -0400

    Made graph search streamlined and oop, no pregeneration

--- 10kindsofpeople.py
+++ 10kindsofpeople.py
@@ -2,30 +2,20 @@ import sys
 
 
 class Queue:
-    def __init__(self, size=1024):
-        self.data = [None] * size
+    def __init__(self):
+        self.data = []
         self.start = 0
         self.end = 0
-        self.size = size
 
     def enq(self, val):
-        if (self.start - self.end) % self.size == 1:
-            new = [self.deq() for _ in range(self.size)] + [None] * self.size
-            self.data = new
-            self.start = 0
-            self.end = 0
-            self.size *= 2
-
-        self.data[self.end] = val
+        self.data.append(val)
         self.end += 1
-        self.end %= self.size
 
     def deq(self):
         if self.is_empty:
             return None
         out = self.data[self.start]
         self.start += 1
-        self.start %= self.size
         return out
 
     @property
@@ -33,33 +23,55 @@ class Queue:
         return self.start == self.end
 
 
-def neighbors(pos, r, c, layout):
-    out = []
-    if pos // c > 0 and layout[pos - c] == layout[pos]:
-        out += [pos - c, ]
-    if pos // c < r - 1 and layout[pos + c] == layout[pos]:
-        out += [pos + c, ]
-    if pos % c > 0 and layout[pos - 1] == layout[pos]:
-        out += [pos - 1, ]
-    if pos % c < c - 1 and layout[pos + 1] == layout[pos]:
-        out += [pos + 1, ]
-    return out
+class Map:
+    def __init__(self, data, dim):
+        self.data = data
+        self.rows = dim[0]
+        self.cols = dim[1]
 
+        self.visited = {}
+        self.visited_all = set()
+        self.zone_map = {}
+        self.max_zone = 0
 
-def search(graph, start, finish, size):
-    visited = set()
-    queue = Queue(size)
-    queue.enq(start)
-    while not queue.is_empty:
-        node = queue.deq()
-        if node == finish:
-            return True
-        adj = graph[node]
-        for n in adj:
-            if n not in visited:
-                queue.enq(n)
-        visited.add(node)
-    return False
+    def match(self, query):
+        start = query[0] * self.cols + query[1]
+        end = query[2] * self.cols + query[3]
+        if self.data[start] != self.data[end]:
+            return 0
+        seen = [start in self.visited_all, end in self.visited_all]
+        if seen[0] and seen[1]:
+            return (self.zone_map[start] == self.zone_map[end]) * (int(self.data[start]) + 1)
+        elif seen[1] or seen[0]:
+            return 0
+        else:
+            return self.search(start, end)
+
+    def search(self, start, end):
+        out = 0
+        zone = self.max_zone
+        self.max_zone += 1
+        queue = self.visited[zone] = Queue()
+        queue.enq(start)
+        while not queue.is_empty:
+            node = queue.deq()
+            if node == end:
+                out = int(self.data[start]) + 1
+            self.visited_all.add(node)
+            self.zone_map[node] = zone
+            adj = node - self.cols
+            if node // self.cols > 0 and adj not in queue.data and self.data[adj] == self.data[node]:
+                queue.enq(adj)
+            adj = node + self.cols
+            if node // self.cols < self.rows - 1 and adj not in queue.data and self.data[adj] == self.data[node]:
+                queue.enq(adj)
+            adj = node - 1
+            if node % self.cols > 0 and adj not in queue.data and self.data[adj] == self.data[node]:
+                queue.enq(adj)
+            adj = node + 1
+            if node % self.cols < self.cols - 1 and adj not in queue.data and self.data[adj] == self.data[node]:
+                queue.enq(adj)
+        return out
 
 
 def main():
@@ -67,19 +79,14 @@ def main():
     kind_map = ""
     for r in range(rows):
         kind_map += sys.stdin.readline().rstrip()
+    area = Map(kind_map, [rows, cols])
 
     n = int(sys.stdin.readline())
     queries = [[int(x) - 1 for x in sys.stdin.readline().split()] for _ in range(n)]
 
-    graph = [neighbors(i, rows, cols, kind_map) for i in range(rows * cols)]
-
     for q in queries:
-        start = q[0] * cols + q[1]
-        end = q[2] * cols + q[3]
-        if search(graph, start, end, rows * cols):
-            print(["binary", "decimal"][int(kind_map[start])])
-        else:
-            print("neither")
+        match = area.match(q)
+        print(["neither", "binary", "decimal"][match])
 
 
 if __name__ == '__main__':
